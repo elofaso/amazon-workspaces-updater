a8ce9661b35f517e46ef177f079479b97aa303f6
Eric Lofaso <elofaso@pm.me>
2021-07-29 00:50:27 -0700

    changed to mindk example

--- examples/main.tf
+++ examples/main.tf
@@ -1,14 +1,14 @@
 module "workspaces" {
   source = "../"
 
-  vpc_id = "vpc-039986426c8bfeaee"
+  vpc_id = "vpc-05f931f32864dd86b"
   vpc_private_subnet_ids = [
-                     "subnet-0e40dbad77458d9a4",
-                     "subnet-0e9618cba036e86ab",
-                     "subnet-0ee191a03e6ebf5c6",
-                     "subnet-0fc67ee4b6714b55d"
+                     "subnet-000c20ecd7312c2a3",
+                     "subnet-033946a105e9307a4",
+                     "subnet-009fab6401c8242ef",
+                     "subnet-0dfae74d5179a48c9"
   ]
-  vpc_default_security_group_id = "sg-0d19d98ea9342b01a"
+  vpc_default_security_group_id = "sg-05fff065869555ec4"
   prototype_directory_password = "hadify9947532ladfaphgsafhoh"
   live_directory_password = "afdahi$eth896394zodfaiadhoha"
   cron_expression = "0 20 * * ? *"
