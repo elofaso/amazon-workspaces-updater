85a2b3dc5f454ff37898c16a3ee78cb0a4dc0db9
Eric Lofaso <elofaso@pm.me>
2021-08-02 00:18:13 -0700

    removed deprecated code

--- step_function.tf
+++ step_function.tf
@@ -1,15 +1,3 @@
-#provider "aws" {
-#  region = "eu-west-1"
-#  #  region = "us-east-1"
-#
-#  # Make it faster by skipping something
-#  skip_get_ec2_platforms      = true
-#  skip_metadata_api_check     = true
-#  skip_region_validation      = true
-#  skip_credentials_validation = true
-#  skip_requesting_account_id  = true
-#}
- 
 module "lambda_layer_python_paramiko" {
   source = "terraform-aws-modules/lambda/aws"
 
